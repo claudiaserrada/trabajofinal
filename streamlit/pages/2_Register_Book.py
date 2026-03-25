6715c41c63263436a04e11096e0733b14f0e5681
claudiaserrada <c.serradadepedraza@hotmail.com>
2026-03-25 08:37:58 +0100

    Update 2_Register_Book.py

--- streamlit/pages/2_Register_Book.py
+++ streamlit/pages/2_Register_Book.py
@@ -13,28 +13,21 @@ if st.button("Guardar libro"):
     if not titulo or not autor or not genero:
         st.error("Todos los campos son obligatorios")
     else:
-       nuevo_libro = {
-    "id": 0,
-    "titulo": titulo,
-    "autor": autor,
-    "genero": genero,
-    "disponible": True
-}
-
-response = requests.post(f"{API_URL}/libros/", json=nuevo_libro)
-
-if response.status_code == 200:
-    st.success("Libro guardado correctamente")
-else:
-    st.error("Error al guardar el libro")
+        nuevo_libro = {
+            "id": 0,
+            "titulo": titulo,
+            "autor": autor,
+            "genero": genero,
+            "disponible": True
+        }
 
         try:
             response = requests.post(f"{API_URL}/libros/", json=nuevo_libro)
 
             if response.status_code == 200:
-                st.success("Libro añadido correctamente")
+                st.success("Libro guardado correctamente")
             else:
-                st.error("Error al añadir libro")
+                st.error("Error al guardar el libro")
 
         except Exception as e:
             st.error(f"Error de conexión: {e}")
