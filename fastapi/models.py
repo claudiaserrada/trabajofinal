100b633cef50e25012ddcb4661f34d82d3f9c9b1
claudiaserrada <c.serradadepedraza@hotmail.com>
2026-03-25 08:54:44 +0100

    Create models.py

---
+++ fastapi/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from database import Base
+
+class LibroDB(Base):
+    __tablename__ = "libros"
+
+    id = Column(Integer, primary_key=True, index=True)
+    titulo = Column(String, nullable=False)
+    autor = Column(String, nullable=False)
+    genero = Column(String, nullable=False)
+    disponible = Column(Boolean, default=True)
