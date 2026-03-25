ffcf68b8b12483844f3407d0a3e90fa5a92dc6d7
claudiaserrada <c.serradadepedraza@hotmail.com>
2026-03-25 09:19:27 +0100

    Update models.py

--- fastapi/models.py
+++ fastapi/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from database import Base
 
+
 class LibroDB(Base):
     __tablename__ = "libros"
 
@@ -9,3 +10,11 @@ class LibroDB(Base):
     autor = Column(String, nullable=False)
     genero = Column(String, nullable=False)
     disponible = Column(Boolean, default=True)
+
+
+class UsuarioDB(Base):
+    __tablename__ = "usuarios"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
