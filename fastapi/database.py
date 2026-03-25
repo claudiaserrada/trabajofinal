f274fc8f013b5a4c1ebcc613af2e54e3cf67b9a2
claudiaserrada <c.serradadepedraza@hotmail.com>
2026-03-25 08:55:13 +0100

    Update database.py

--- fastapi/database.py
+++ fastapi/database.py
@@ -1,68 +1,11 @@
-from fastapi import FastAPI, Depends
-from pydantic import BaseModel as PydanticBaseModel
-from typing import List
-from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-from database import SessionLocal, engine, Base
-from models import LibroDB
+DATABASE_URL = "sqlite:///./biblioteca.db"
 
-Base.metadata.create_all(bind=engine)
-
-class Libro(PydanticBaseModel):
-    id: int | None = None
-    titulo: str
-    autor: str
-    genero: str
-    disponible: bool = True
-
-class ListadoLibros(PydanticBaseModel):
-    libros: List[Libro] = []
-
-app = FastAPI(
-    title="Gestor de Bibliotecas API",
-    description="Servidor de datos para la gestión de bibliotecas.",
-    version="1.0.0",
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-@app.get("/libros/")
-def retrieve_data(db: Session = Depends(get_db)):
-    libros_db = db.query(LibroDB).all()
-    libros = [
-        {
-            "id": libro.id,
-            "titulo": libro.titulo,
-            "autor": libro.autor,
-            "genero": libro.genero,
-            "disponible": libro.disponible,
-        }
-        for libro in libros_db
-    ]
-    return {"libros": libros}
-
-@app.post("/libros/")
-def create_book(libro: Libro, db: Session = Depends(get_db)):
-    nuevo_libro = LibroDB(
-        titulo=libro.titulo,
-        autor=libro.autor,
-        genero=libro.genero,
-        disponible=True
-    )
-    db.add(nuevo_libro)
-    db.commit()
-    db.refresh(nuevo_libro)
-
-    return {
-        "mensaje": "Libro añadido correctamente",
-        "id": nuevo_libro.id
-    }
-
-@app.post("/prestamos/")
-async def create_loan(libro_id: int):
-    return {"message": "Préstamo creado (no realmente)", "libro_id": libro_id}
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
